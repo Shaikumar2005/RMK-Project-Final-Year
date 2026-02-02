1136a22df5337ea91ed5466ef10f8dc421070a8b
Shaikumar R <shaikumar.r@mphasis.com>
2026-02-02 14:36:00 +0530

    Updated work

--- src/main/java/com/rmkit/example/demo/repo/UserRepository.java
+++ src/main/java/com/rmkit/example/demo/repo/UserRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsernameIgnoreCase(String username);
     boolean existsByUsernameIgnoreCase(String username);
     boolean existsByEmailIgnoreCase(String email);
 }
+
+
